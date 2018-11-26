561e2d38731a0329898141485d6976bbb719461c
Alfan <alfan2305@yahoo.com>
2018-11-27 01:50:47 +0700

    fix typo

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ environments.production = {
 }
 
 // Determine which environment was passed as a command-line argument
-const currentEnvironment = typeof(prosess.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+const currentEnvironment = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
 
 // Check that the current environment is one of the environment above, if not default to staging
 const environmentToExport = typeof(environments[currentEnvironment]) == 'object' ? environments[currentEnvironment] : environments.staging;
