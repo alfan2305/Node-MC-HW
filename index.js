a0338bd8bd927ac16ab67216eb14e275b352771f
Alfan <alfan2305@live.com>
2019-03-24 19:56:56 +0700

    update queryStringObject

--- index.js
+++ index.js
@@ -4,17 +4,17 @@
  */
 
 // Dependencies
-const http = require('http');
-const https = require('https');
-const url = require('url');
-const StringDecoder = require('string_decoder').StringDecoder;
-const config = require('./config');
-const fs = require('fs');
+const http = require("http");
+const https = require("https");
+const url = require("url");
+const StringDecoder = require("string_decoder").StringDecoder;
+const config = require("./config");
+const fs = require("fs");
 
 // Instantiate the HTTP server
 const httpServer = http.createServer(function(req, res) {
   unifiedServer(req, res);
-})
+});
 
 // Start the HTTP server
 httpServer.listen(config.httpPort, function() {
@@ -23,13 +23,13 @@ httpServer.listen(config.httpPort, function() {
 
 // Instantiate the HTTPS server
 const httpsServerOptions = {
-  'key' : fs.readFileSync('./https/key.pem'),
-  'cert' : fs.readFileSync('./https/cert.pem')
+  key: fs.readFileSync("./https/key.pem"),
+  cert: fs.readFileSync("./https/cert.pem")
 };
 
 const httpsServer = http.createServer(httpsServerOptions, function(req, res) {
   unifiedServer(req, res);
-})
+});
 
 // Start the HTTPS server
 httpsServer.listen(config.httpsPort, function() {
@@ -43,7 +43,7 @@ const unifiedServer = function(req, res) {
 
   // Get the path
   const path = parseUrl.pathname;
-  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+  const trimmedPath = path.replace(/^\/+|\/+$/g, "");
 
   // Get the query string as an object
   const queryStringObject = parseUrl.query;
@@ -55,38 +55,41 @@ const unifiedServer = function(req, res) {
   const headers = req.headers;
 
   // Get the payload, if any
-  const decoder = new StringDecoder('utf-8');
-  let buffer = '';
+  const decoder = new StringDecoder("utf-8");
+  let buffer = "";
 
-  req.on('data', function(data) {
+  req.on("data", function(data) {
     buffer += decoder.write(data);
-  })
+  });
 
-  req.on('end', function() {
+  req.on("end", function() {
     buffer += decoder.end();
 
     // Check the router for a matching path for a handler. If one is not found, use the notFound handler instead.
-    const chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+    const chosenHandler =
+      typeof router[trimmedPath] !== "undefined"
+        ? router[trimmedPath]
+        : handlers.notFound;
 
     // Construct the data object to send to the handler
     const data = {
-      'trimmedPath' : trimmedPath,
-      'queryStringObject' : queryStringObject,
-      'method' : method,
-      'headers' : headers,
-      'payload' : buffer
+      trimmedPath: trimmedPath,
+      queryStringObject: queryStringObject,
+      method: method,
+      headers: headers,
+      payload: buffer
     };
 
     // Route the request to the handler specified in the router
     chosenHandler(data, function(statusCode, payload) {
       // Use the payload returned from the handler, or set the default payload to an empty object
-      payload = typeof(payload) == 'object' ? payload : {};
+      payload = typeof payload == "object" ? payload : {};
 
       // Convert the payload to a string
       const payloadString = JSON.stringify(payload);
 
       // Return the response
-      res.setHeader('Content-Type', 'application/json');
+      res.setHeader("Content-Type", "application/json");
       res.writeHead(statusCode);
       res.end(payloadString);
       console.log(trimmedPath, statusCode);
@@ -104,7 +107,13 @@ handlers.ping = function(data, callback) {
 
 // Hello handler
 handlers.hello = function(data, callback) {
-  callback(200, {message: 'Hello U 😃'});
+  console.log(data);
+  callback(200, {
+    message: `Hello ${
+      data.queryStringObject.name !== undefined ? data.queryStringObject.name : ""
+    } 😃`,
+    language: data.queryStringObject.lang !== undefined ? data.queryStringObject.lang : ""
+  });
 };
 
 // Not-Found handler
@@ -114,6 +123,6 @@ handlers.notFound = function(data, callback) {
 
 // Define the request router
 const router = {
-  'ping' : handlers.ping,
-  'hello' : handlers.hello
+  ping: handlers.ping,
+  hello: handlers.hello
 };
